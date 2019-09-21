32125723c526bcd0827f5bd5cf463dc0cf7c6984
Alina Suon <alinasuon@gmail.com>
2019-09-21 06:27:37 -0700

    Update function.m

--- FourierCoef&Trig/function.m
+++ FourierCoef&Trig/function.m
@@ -13,14 +13,3 @@ end
     
 
 
-
-%lab 4
-% function dydx = f(t,S,deriv) 
-%     if (deriv == 1) 
-%         dydx = (1/4) - (3/500)*S;
-%     end
-%     if (deriv == 2)
-%         dydx = (3/2500)* exp(-3*t/500);
-%     end
-% end
-
